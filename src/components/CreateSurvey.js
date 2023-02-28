a8b51eaad7fd1c386cfda146492509748b574921
Leela0123 <117524740+Leela0123@users.noreply.github.com>
2023-02-28 14:39:52 +0530

    Update CreateSurvey.js

--- src/components/CreateSurvey.js
+++ src/components/CreateSurvey.js
@@ -5,13 +5,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 
 const CreateSurvey = ({ squestions, setSquestions }) => {
+  // console.log("sQue", squestions, " ", "SetQ", setSquestions);
   const history = useNavigate();
   const getRandom = () => {
     return Math.floor(Math.random() * 1000 + 1);
   };
   const defaultOptionsState = [
     { uid: getRandom(), value: "" },
-    { uid: getRandom(), value: "" },
+   
   ];
   const [qText, setQtext] = useState("");
   const [qType, setQtype] = useState(0);
@@ -23,13 +24,14 @@ const CreateSurvey = ({ squestions, setSquestions }) => {
       value: "",
     };
     let updatedOptions = [...options];
+    // console.log("Updated Option: ", updatedOptions);
     updatedOptions.push(newOption);
     setOptions(updatedOptions);
   };
 
   const deleteOptions = () => {
-    if (options.length === 2) {
-      alert("Error: A select type question should have atleast 2 options");
+    if (options.length === 1) {
+      alert("Error: A select type question should have atleast 1 options");
     } else {
       let updatedOptions = [...options];
       updatedOptions.pop();
@@ -40,11 +42,16 @@ const CreateSurvey = ({ squestions, setSquestions }) => {
   const updateOptionText = (id, text) => {
     let updatedOptions = [...options];
     let changeIndex = updatedOptions.findIndex((x) => x.uid === id);
+    // console.log("changedIndex", changeIndex);
     updatedOptions[changeIndex].value = text;
     setOptions(updatedOptions);
   };
 
   const updateSurveyQuestion = () => {
+    if (qText === "") {
+      alert("Fill the Data");
+      return;
+    }
     let newSurveyQuestion = [...squestions];
     let newQ = {
       qtext: qText,
@@ -53,9 +60,9 @@ const CreateSurvey = ({ squestions, setSquestions }) => {
     };
     newSurveyQuestion.push(newQ);
     setSquestions(newSurveyQuestion);
-    setQtype(0);
-    setQtext("");
-    setOptions(defaultOptionsState);
+    setQtype(0); //reset
+    setQtext(""); //reset
+    setOptions(defaultOptionsState); //reset
   };
 
   const publish = () => {
@@ -64,13 +71,14 @@ const CreateSurvey = ({ squestions, setSquestions }) => {
   };
 
   return (
-    <div className="container">
+    <div className="createSurvey">
       <TypeSelector qtype={qType} setQtype={setQtype} />
 
       {qType !== 0 ? (
         <>
           <Question onTextUpdate={setQtext} />
 
+          {/* Options Code  */}
           {options.map((opt, key) => (
             <Options
               key={key}
@@ -80,15 +88,27 @@ const CreateSurvey = ({ squestions, setSquestions }) => {
               updateText={updateOptionText}
             />
           ))}
-          <button
-            className="btn btn-primary m-1"
-            onClick={() => updateSurveyQuestion()}
-          >
-            Add Question
-          </button>
-          <button className="btn btn-primary m-1" onClick={() => publish()}>
-            Publish
-          </button>
+          {qType === 1 ? (
+            options.length >= 4 ? (
+              <>
+                <button className="btn1" onClick={() => updateSurveyQuestion()}>
+                  Add Question
+                </button>
+                <button className="btn1" onClick={() => publish()}>
+                  Publish
+                </button>
+              </>
+            ) : null
+          ) : qType === 2 ? (
+            <>
+              <button className="btn1" onClick={() => updateSurveyQuestion()}>
+                Add Question
+              </button>
+              <button className="btn1" onClick={() => publish()}>
+                Publish
+              </button>
+            </>
+          ) : null}
         </>
       ) : null}
     </div>
